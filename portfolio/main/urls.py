f5c01f477cb1861fc9af7e7a0710098e88473713
Leif Anderson <leif.v.anderson@gmail.com>
2017-07-17 10:34:19 -0500

    1st Portfolio done

--- portfolio/main/urls.py
+++ portfolio/main/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('apps.portfolio.urls'))
+    #Wurl(r'^$', include('apps.portfolio.urls'))
+    url(r'^', include('apps.portfolio.urls'))
+
 ]
